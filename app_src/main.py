ada788ae614eddbe70849b12d09b6e15758c7619
Dorstol <olehsofronov@gmail.com>
2025-04-29 12:50:15 +0000

    add prometheus metrics

--- app_src/main.py
+++ app_src/main.py
@@ -1,10 +1,13 @@
 import logging
+import time
 
 import uvicorn
+from fastapi import Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from api import router as api_router
 from core.config import settings
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 from create_fastapi_app import create_app
 
 logging.basicConfig(
@@ -16,11 +19,34 @@ main_app = create_app(
     create_custom_static_urls=True,
 )
 
+REQUEST_COUNT = Counter(
+    "fastapi_requests_total", "Total number of requests", ["method", "endpoint"]
+)
+
+REQUEST_LATENCY = Histogram(
+    "fastapi_request_duration_seconds", "Request latency", ["endpoint"]
+)
+
+
 main_app.mount("/static", StaticFiles(directory="static"), name="static", )
 
 main_app.include_router(
     api_router,
 )
+
+@main_app.middleware("http")
+async def metrics_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+
+    REQUEST_COUNT.labels(request.method, request.url.path).inc()
+    REQUEST_LATENCY.labels(request.url.path).observe(process_time)
+    return response
+
+@main_app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 main_app.add_middleware(
     CORSMiddleware,
